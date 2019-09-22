1e42d3e9c4d099967a9d695cb397c254ebccc837
Fabiano Gonzaga <f.augusto.g@gmail.com>
2019-09-22 15:38:06 -0300

    teste

--- MGonzaga.IoC.NETCore.WebAPI/Controllers/v1/UserController.cs
+++ MGonzaga.IoC.NETCore.WebAPI/Controllers/v1/UserController.cs
@@ -133,7 +133,7 @@ namespace MGonzaga.IoC.NETCore.WebAPI.Controllers.v1
         [HttpPut("{linkUniqueId}/confirm-email")]
         public ActionResult<string> PutConfirmEmail(string linkUniqueId, [FromBody] ConfirmPasswordViewModel value)
         {
-            return Ok("Funcionou - Teste alteração Fabiano");
+            return Ok("Funcionou - Teste alteração Fabiano 12");
         }
         /// <summary>
         /// Change user password
